e4f8a9d1092c2107984210e2b20d2730b98af319
Rebecca Richardson <rebecca.s.richardson4@gmail.com>
2021-03-24 09:48:45 -0500

    css and js added

---
+++ etch-a-sketch.js
@@ -0,0 +1,11 @@
+// Select the elements on the page - canvas, shake button
+
+// Setup canvas for drawing
+
+// write a draw function
+
+// wite a handler for the keys
+
+// clear /shake function
+
+// listen for arrow keys
