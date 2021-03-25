7b864d6f3349d5255fee60d98ebb8f929ce16c91
Rebecca Richardson <rebecca.s.richardson4@gmail.com>
2021-03-25 10:16:09 -0500

    arrows draw lines through switch statement

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,17 +2,18 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 10;
 
 // Setup canvas for drawing
 const { width, height } = canvas; // make a variable called height and width from the same properties on our canvas
-const x = Math.floor(Math.random() * width);
-const y = Math.floor(Math.random() * height);
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
 
 // create random x and y starting points on the canvas
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 
 ctx.beginPath(); // start the drawing
 ctx.moveTo(x, y);
@@ -20,13 +21,37 @@ ctx.lineTo(x, y);
 ctx.stroke();
 
 // write a draw function
+function draw({ key }) {
+  console.log(key);
+  // start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // move x and y values depending on what user did
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
 
 // wite a handler for the keys
 function handleKey(e) {
   if (e.key.includes('Arrow')) {
     e.preventDefault();
-    console.log(e.key);
-    console.log('HANDLING KEY');
+    draw({ key: e.key });
   }
 }
 
