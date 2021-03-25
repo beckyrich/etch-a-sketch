429990ec627b4378d9b37e20c813c026f432dfbc
Rebecca Richardson <rebecca.s.richardson4@gmail.com>
2021-03-24 20:07:49 -0500

    key handler added

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,6 +6,7 @@ const shakebutton = document.querySelector('.shake');
 // Setup canvas for drawing
 const { width, height } = canvas; // make a variable called height and width from the same properties on our canvas
 const x = Math.floor(Math.random() * width);
+const y = Math.floor(Math.random() * height);
 
 // create random x and y starting points on the canvas
 
@@ -14,14 +15,22 @@ ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 
 ctx.beginPath(); // start the drawing
-ctx.moveTo(x, 200);
-ctx.lineTo(x, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
 
 // write a draw function
 
 // wite a handler for the keys
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    console.log(e.key);
+    console.log('HANDLING KEY');
+  }
+}
 
 // clear /shake function
 
 // listen for arrow keys
+window.addEventListener('keydown', handleKey);
