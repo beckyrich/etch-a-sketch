38aa6680daa98cee48b5bcd310b64e74a74e109f
Rebecca Richardson <rebecca.s.richardson4@gmail.com>
2021-03-27 19:17:57 -0400

    shake button functionality and clear

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
-const MOVE_AMOUNT = 10;
+const MOVE_AMOUNT = 20;
 
 // Setup canvas for drawing
 const { width, height } = canvas; // make a variable called height and width from the same properties on our canvas
@@ -25,7 +25,7 @@ ctx.stroke();
 // write a draw function
 function draw({ key }) {
   // increment the hue
-  hue += 1;
+  hue += 5;
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   console.log(key);
   // start the path
@@ -61,6 +61,18 @@ function handleKey(e) {
 }
 
 // clear /shake function
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    'animationend',
+    function () {
+      canvas.classList.remove('shake');
+    },
+    { once: true }
+  );
+}
 
 // listen for arrow keys
 window.addEventListener('keydown', handleKey);
+shakebutton.addEventListener('click', clearCanvas);
